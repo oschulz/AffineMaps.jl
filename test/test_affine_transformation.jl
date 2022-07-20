c078927ab4232e85257f5a60e23327009294c1c2
Oliver Schulz <oschulz@mpp.mpg.de>
2022-07-20 21:36:01 +0200

    Initial package layout

---
+++ test/test_affine_transformation.jl
@@ -0,0 +1,9 @@
+# This file is a part of AffineTransformations.jl, licensed under the MIT License (MIT).
+
+using AffineTransformations
+using Test
+
+
+@testset "hello_world" begin
+    @test AffineTransformations.hello_world() == 42
+end
